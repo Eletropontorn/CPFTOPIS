c7ab3bff1a4733829ffa4cd2a54e72beec5c20b9
Eletroponto RN - Controle de Ponto e Acesso <162372558+Eletropontorn@users.noreply.github.com>
2024-09-04 11:23:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,20 +9,17 @@ document.getElementById('pisForm').addEventListener('submit', function(event) {
   }
 
   let soma = 0;
-  const pesos = [3, 2, 9, 8, 7, 6, 5, 4, 3, 2]; // números multiplicadores conforme a fórmula
+  const pesos = [3, 2, 9, 8, 7, 6, 5, 4, 3, 2]; // Pesos usados no cálculo
 
   for (let i = 0; i < 10; i++) {
     soma += parseInt(pis.charAt(i)) * pesos[i];
   }
 
-  let digitoVerificador = soma % 11;
-
-  if (digitoVerificador < 2 || digitoVerificador === 10) {
-    digitoVerificador = 0; // Se o resultado for menor que 2 ou igual a 10, o dígito verificador será 0
-  } else {
-    digitoVerificador = 11 - digitoVerificador; // Calculando o dígito verificador
+  let digitoVerificador = 11 - (soma % 11);
+  if (digitoVerificador === 10 || digitoVerificador === 11) {
+    digitoVerificador = 0; // Se o dígito verificador for 10 ou 11, ele deve ser 0
   }
 
-  // Exibindo o CPF completo com o dígito verificador
+  // Exibindo o PIS/PASEP completo com o dígito verificador
   document.getElementById('resultado').innerText = `O PIS/PASEP completo é: ${pis}${digitoVerificador}`;
 });
